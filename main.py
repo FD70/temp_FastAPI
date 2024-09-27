31bfbab8d861f1e28e0ba887bbd5420bcfa58578
FD70000000 <halfl.ez.ser@mail.ru>
2024-09-27 03:50:04 +0300

    todo: add .delete | raise exceptions instead PlainText

--- main.py
+++ main.py
@@ -9,25 +9,44 @@ LETTERS = string.ascii_letters
 
 BASE_URL = "http://127.0.0.1:8000"
 
-todoData = {
-    "id": 1,
-    "item": ''.join(random.choice(LETTERS) for _ in range(4))
-}
+
+def _get_random_todo_data() -> dict:
+    return {
+        "id": random.randint(1, 100),
+        "item": ''.join(random.choice(LETTERS) for _ in range(7))
+    }
+
+
+def r_get_anything(route: str) -> requests.Response:
+    return requests.get(BASE_URL + route)
 
 
 def r_get_todo() -> requests.Response:
     return requests.get(BASE_URL + "/todo")
 
 
+def r_get_todo_by_id(todo_id: int) -> requests.Response:
+    return requests.get(BASE_URL + f"/todo/{todo_id}")
+
+
 def r_post_todo(todo: dict) -> requests.Response:
     return requests.post(BASE_URL + "/todo", data=json.dumps(todo))
 
 
-def r_put_todo(item_id: int, todo_item: dict) -> requests.Response:
-    return requests.put(BASE_URL + f"/todo/{item_id}", data=json.dumps(todo_item))
+def r_put_todo(todo_id: int, todo_item: dict) -> requests.Response:
+    return requests.put(BASE_URL + f"/todo/{todo_id}", data=json.dumps(todo_item))
+
+
+def r_delete_todo(todo_id: int) -> requests.Response:
+    return requests.delete(BASE_URL + f"/todo/{todo_id}")
+
+
+def r_delete_all() -> requests.Response:
+    return requests.delete(BASE_URL + "/todo/all")
 
 
 if __name__ == '__main__':
 
-    print(r_put_todo(1, todoData).text)
-    print(r_get_todo().text)
+    req = r_post_todo(_get_random_todo_data())
+    print(req.text)
+    print(req.status_code)
--- rout/todo.py
+++ rout/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, HTTPException, status, responses
 
 import model
 
@@ -9,7 +9,8 @@ todo_list = []
 ##
 MESSAGE = "message"
 
-@todo_router.post("/todo")
+
+@todo_router.post("/todo", status_code=201)
 async def add_todo(data: model.Todo) -> dict:
     todo_list.append(data)
     return {MESSAGE: "Todo added succesfully"}
@@ -27,9 +28,13 @@ async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to 
             return {
                 "todo": _todo
             }
-    return {
-        MESSAGE: "Todo with supplied ID doesn't exist."
-    }
+    # return responses.PlainTextResponse(
+    #     str({MESSAGE: "Todo with supplied ID doesn't exist."}),
+    #     status_code=status.HTTP_404_NOT_FOUND)
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist."
+    )
 
 
 @todo_router.put("/todo/{todo_id}")
@@ -41,6 +46,30 @@ async def update_todo(todo_data: model.TodoItem, todo_id: int = Path(..., title=
             return {
                 MESSAGE: "Todo updated successfully."
             }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist."
+    )
+
+
+@todo_router.delete("/todo/all")
+async def delete_all_todo() -> dict:
+    todo_list.clear()
     return {
-        MESSAGE: "Todo with supplied ID doesn't exist."
+        MESSAGE: "Todos deleted successfully."
     }
+
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_single_todo(todo_id: int) -> dict:
+    for i in range(len(todo_list)):
+        _todo = todo_list[i]
+        if _todo.id == todo_id:
+            todo_list.pop(i)
+            return {
+                MESSAGE: f"Todo with id:{todo_id} deleted successfully."
+            }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Todo with id:{todo_id} doesn't exist."
+    )
